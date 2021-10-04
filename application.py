810a83e68845650682ea29b02ed8136faf131733
ismailkoussa <ismailkoussa2019@gmail.com>
2021-10-04 16:54:11 +0300

    uploading the first version of the final project

---
+++ application.py
@@ -0,0 +1,189 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///items.db")
+
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+#.....................................
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    if request.method == "POST":
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        rows = db.execute("SELECT * FROM users WHERE username = ?", username)
+
+        # Ensure the username was submitted
+        if not username:
+            return apology("must provide username", 400)
+        # Ensure the username doesn't exists
+        elif len(rows) != 0:
+            return apology("username already exists", 400)
+
+        # Ensure password was submitted
+        elif not password:
+            return apology("must provide password", 400)
+
+        # Ensure confirmation password was submitted
+        elif not request.form.get("confirmation"):
+            return apology("must provide a confirmation password", 400)
+
+        # Ensure passwords match
+        elif not password == confirmation:
+            return apology("Incorrect confirmation", 400)
+
+
+        else:
+
+            # Insert the new user
+            db.execute(
+                "INSERT INTO users (username, password) VALUES (?, ?) ", username, password,
+            )
+            # Redirect user to home page
+            return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+#...................................
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username!!", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not password == confirmation:
+            return apology("invalid username and/or password.....", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+#...........................................................
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+#..........................................
+
+
+@app.route("/buy", methods=["GET", "POST"])
+
+def buy():
+    """Buy shares of stock"""
+    if request.method == "POST":
+        symbol = request.form.get("symbol")
+        shares = request.form.get("shares")
+        price = db.execute(
+            "SELECT price FROM item WHERE name = ? ", symbol)
+
+
+        current = db.execute(
+            "SELECT cash FROM users WHERE id = ? ", session["user_id"]
+        )[0]["cash"]
+
+        value = shares * price
+
+        if current < (value):
+            return apology("no enough cash",400)
+        db.execute("insert into stock (user_id, item, quantity, price) values(?,?,?,?,?)",
+        session["user_id"],symbol,shares,value
+        )
+        db.execute(
+            "UPDATE users SET cash = cash - ? WHERE id = ?",
+            value,
+            session["user_id"],
+        )
+        return redirect("/buy")
+
+    else:
+        return render_template("buy.html")
+#....................................................................
+
+
+@app.route("/bill")
+
+def bill():
+    total = 0
+    stock = db.execute("SELECT * FROM stock WHERE user_id = ?", session["user_id"])
+    for stockmin in stock:
+        total += stockmin["price"]
+
+    return render_template("bill.html", stock=stock,total=total)
